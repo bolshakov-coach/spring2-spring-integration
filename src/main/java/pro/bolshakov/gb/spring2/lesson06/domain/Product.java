d82eb3457ec5d0a3ca6a53763714b418a1ad9342
Aleksandr <mail.bolshakov@gmail.com>
2020-12-22 19:04:35 +0300

    to string for product

--- src/main/java/pro/bolshakov/gb/spring2/lesson06/domain/Product.java
+++ src/main/java/pro/bolshakov/gb/spring2/lesson06/domain/Product.java
@@ -39,4 +39,13 @@ public class Product {
     public void setPrice(Double price) {
         this.price = price;
     }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
